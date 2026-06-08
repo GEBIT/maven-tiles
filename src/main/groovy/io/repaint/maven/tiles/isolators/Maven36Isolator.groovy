18ebe4f44e4d54fd45e2d3b5e4355f3c765f9f86
Carsten Pfeiffer <carsten.pfeiffer@gebit.de>
2026-06-08 15:31:24 +0200

    GBLD-2186: WIP: flush the mvnd TimestampCacheFactory$TimestampCache
    We will probably not need this, so this is just a "backup with explanation".
    The cache-flushing was introduced in bf5c94321e02784716bd3454fab426b0ec11497b
    for the case where, in m2e, a tile adds further dependencies and the cache must
    be updated.
    
    mvnd installs a different cache, which we handle in this commit.
    
    However, mvnd is not used in eclipse/m2e (and probably never will). And from
    experiments, the cache is always empty when flushProjectArtifacts() is invoked,
    both in plain commandline mvn and mvnd, even when a tile adds another dependency.
    
    So we will not merge this into master.

--- src/main/groovy/io/repaint/maven/tiles/isolators/Maven36Isolator.groovy
+++ src/main/groovy/io/repaint/maven/tiles/isolators/Maven36Isolator.groovy
@@ -4,6 +4,7 @@ import groovy.transform.CompileStatic
 import groovy.transform.TypeCheckingMode
 
 import java.io.File
+import java.util.function.BiPredicate
 
 import org.apache.maven.MavenExecutionException
 import org.apache.maven.execution.MavenSession
@@ -32,15 +33,37 @@ class Maven36Isolator extends AetherIsolator {
 		projectArtifactsCache = mavenSession.container.lookup(projectArtifactsCacheClass)
 	}
 
+	// m2e: flush cached artifacts in case a *tile* adds further dependencies. Does not happen in plain Maven,
+	// nor in mvnd.
 	void flushProjectArtifacts(MavenProject project) {
-		projectArtifactsCache.cache.iterator().with { iterator ->
-			iterator.each { entry ->
-				if (entry.hasProperty('key')) {
-					def cacheKey = entry.key
-					if (cacheKey.groupId == project.groupId
-							&& cacheKey.artifactId == project.artifactId
-							&& cacheKey.version == project.version) {
-						iterator.remove()
+		if (projectArtifactsCache.cache == null) {
+			return
+		}
+		// Support for mvnd's TimestampCache
+		if (projectArtifactsCache.cache.getMetaClass().respondsTo(
+				projectArtifactsCache.cache,
+				'removeIf',
+				BiPredicate)) {
+				// System.err.println("mmmmmmmmmmmmmmmmmmmmmmmmm " + projectArtifactsCache.cache.map.size())
+				// Note: we don't catch any exceptions here so that we immediately notice when it breaks.
+				projectArtifactsCache.cache.removeIf { k, v ->
+				throw new RuntimeException(k)
+					k.groupId == project.groupId &&
+					k.artifactId == project.artifactId &&
+					k.version == project.version
+				}
+		} else {
+			// Maven's DefaultProjectArtfifactsCache
+			// System.err.println("bbbbbbbbbbbbbbbbbbbbbbbb " + projectArtifactsCache.cache.size())
+			projectArtifactsCache.cache.iterator().with { iterator ->
+				iterator.each { entry ->
+					if (entry.hasProperty('key')) {
+						def cacheKey = entry.key
+						if (cacheKey.groupId == project.groupId
+								&& cacheKey.artifactId == project.artifactId
+								&& cacheKey.version == project.version) {
+							iterator.remove()
+						}
 					}
 				}
 			}
